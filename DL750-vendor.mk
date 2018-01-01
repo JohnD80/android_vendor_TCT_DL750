50b45528523855bc4e5f83a0d60e625d100f588d
kirito96 <eagle1968@live.com>
2018-01-01 14:26:25 -0400

    Port to DL750 from Yaris_M_GSM

---
+++ DL750-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/TCT/DL750/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/TCT/DL750/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/TCT/DL750/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/TCT/DL750/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/TCT/DL750/proprietary/xbin,system/xbin)
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# android_vendor_TCT_DL750
+This vendor tree is for TCT DL750,MT6572.
